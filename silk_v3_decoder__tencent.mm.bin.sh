75fa5ace056d82fdc7c55c494f0e567744812cbb
Your Name <you@example.com>
2019-04-28 08:08:39 +0800

    fix the script , add lame

--- silk_v3_decoder__tencent.mm.bin.sh
+++ silk_v3_decoder__tencent.mm.bin.sh
@@ -3,7 +3,7 @@
 usage(){
     echo
     echo
-    echo
+    echo "$0 <input.amr>"
     echo
     echo
     exit
@@ -14,5 +14,21 @@ usage(){
 echo "silk_v3_decoder__tencent.mm.bin $1"
       silk_v3_decoder__tencent.mm.bin $1
 
-echo "ffmpeg -f s16le -ar 24000 -ac 1 -i tmp.s16le.24000.ac1.pcm  $1.wav"
-      ffmpeg -f s16le -ar 24000 -ac 1 -i tmp.s16le.24000.ac1.pcm  $1.wav
+outPut=$(basename $1).wav
+
+echo "ffmpeg -f s16le -ar 24000 -ac 1 -i tmp.s16le.24000.ac1.pcm  ${outPut}"
+      ffmpeg -f s16le -ar 24000 -ac 1 -i tmp.s16le.24000.ac1.pcm  ${outPut}
+
+echo "rm -f tmp.s16le.24000.ac1.pcm"
+      rm -f tmp.s16le.24000.ac1.pcm
+
+lameBin=$(which lame)
+if [ -n "${lameBin}" ]
+then
+
+    echo "${lameBin}     ${outPut}    ${outPut}.mp3"
+          ${lameBin}     ${outPut}    ${outPut}.mp3
+
+    echo "rm -f      ${outPut}    "
+          rm -f      ${outPut}    
+fi
